13adfbe2f2db521aaf9925813d07132caa987016
Azahari Zaman <azaharizaman@gmail.com>
2023-11-26 03:40:38 +0800

    Enable support for Emotion Classifications
    Signed-off-by: Azahari Zaman <azaharizaman@gmail.com>

---
+++ src/Responses/Inference/CreateResponseEmotionClassification.php
@@ -0,0 +1,30 @@
+<?php namespace AzahariZaman\Huggingface\Responses\Inference;
+
+use AzahariZaman\Huggingface\Contracts\ResponseContract;
+use AzahariZaman\Huggingface\Responses\Concerns\ArrayAccessible;
+
+final class CreateResponseEmotionClassification implements ResponseContract
+{
+    use ArrayAccessible;
+    public function __construct(
+        private readonly array $results)
+    {
+        // ..
+    }
+
+    public static function from(array $attributes)
+    {
+        return new self($attributes);
+    }
+
+    public function toArray(): array
+    {
+        $results = [];
+
+        foreach ($this->results as $key => $result) {
+            $results[$result['label']] = $result['score'];
+        }
+
+        return $results;
+    }
+}
